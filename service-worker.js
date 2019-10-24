e950711d67d1745488f12f4a7753df8da10f9510
GitHub <noreply@github.com>
2019-10-24 18:38:14 +0000

    e5355b4 chore(deps): upgrade d2-i18n(-generate) deps (master) (#155)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.20f1d21b68adb3e781bc2c4ed9254eff.js"
+  "./precache-manifest.41d6408fd7648004ecbc0822e86b4b16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
