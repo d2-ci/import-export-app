f17947088210cdb0fb29c6236c75254d7cc15a7c
GitHub <noreply@github.com>
2020-09-08 22:23:59 +0000

    1bb56b6 fix(ui): styling adjustments according to ui-review(#668)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eb4b2cfe01688b4c176ce2bb2f1ace92.js"
+  "./precache-manifest.2b82b1ab7807349760435736a0bfdcc6.js"
 );
 
 self.addEventListener('message', (event) => {
