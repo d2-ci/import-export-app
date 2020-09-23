e1576de786d12ab15c3ce31721e935defc9d7739
GitHub <noreply@github.com>
2020-09-23 13:13:37 +0000

    de37f51 chore(deps-dev): bump query-string from 6.12.1 to 6.13.2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.12c85fffaaafad77d11566f273a103e8.js"
+  "./precache-manifest.ba437cb40d6b975f927b96bf5e5d2f4f.js"
 );
 
 self.addEventListener('message', (event) => {
