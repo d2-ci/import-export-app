189959208cf148703d6f2aeb38b6cac612f4bf8e
GitHub <noreply@github.com>
2020-10-28 01:04:43 +0000

    d9137e2 fix(translations): sync translations from transifex (v34)

--- precache-manifest.f18ece93815115f16833b9153499176a.js
+++ precache-manifest.f18ece93815115f16833b9153499176a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2cc655e117f98f44b255d52ac7be4695",
+    "revision": "c955c11b551247e13b936dadcd0e5214",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.bbfd1532.chunk.css"
   },
   {
-    "revision": "627c9cbfe23e0477c17f",
+    "revision": "447bf5cb6b28fb7e8c92",
     "url": "./static/css/main.0e8df5a1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3b82e566.chunk.js"
   },
   {
-    "revision": "627c9cbfe23e0477c17f",
-    "url": "./static/js/main.5d0eb8a7.chunk.js"
+    "revision": "447bf5cb6b28fb7e8c92",
+    "url": "./static/js/main.0aaf2446.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9c12a227073b278b09f4e1b7b41ec094.js"
+  "./precache-manifest.f18ece93815115f16833b9153499176a.js"
 );
 
 self.addEventListener('message', (event) => {
