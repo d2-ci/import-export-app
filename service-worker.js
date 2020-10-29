b39b197ade49af711307f4661c5e40f988b1a741
GitHub <noreply@github.com>
2020-10-29 01:15:32 +0000

    382bceb fix(translations): sync translations from transifex (v33)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2f2139a44daff228c54ef0eceae8b8b9.js"
+  "./precache-manifest.76ae0c61a8fc2cd8c5f526f3c0a09e6a.js"
 );
 
 self.addEventListener('message', (event) => {
